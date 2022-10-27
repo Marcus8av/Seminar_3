d5f40703003b2f3eb10b371aa42b16d860bccb75
markus b <mnogo_bukof@mail.ru>
2022-10-27 23:29:29 +0700

    Первое задание

---
+++ Задание 1/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Напишите программу, которая принимает на вход пятизначное число
+и проверяет, является ли оно палиндромом*/
+
+void palindrome(string pal)
+{
+    if (pal[0] == pal[4] && pal[1] == pal[3])
+    Console.WriteLine($"Введенное число это палиндром");
+    else
+    Console.WriteLine($"Введеное число не палиндром");
+}
+
+Console.WriteLine("Ввeдите пятизначное число: ");
+
+int Number = Convert.ToInt32(Console.ReadLine());
+string str = Convert.ToString(Number);
+
+palindrome(str);
