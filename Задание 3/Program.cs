d6ba1d19197e7df2bf2765b92ecdb5e8b6e3aa01
markus b <mnogo_bukof@mail.ru>
2022-10-27 23:30:32 +0700

    третье  задание

---
+++ Задание 3/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Напишите программу, которая принимает на вход число (N)
+и выдаёт таблицу кубов чисел от 1 до N*/
+
+void cube(int N)
+{
+    int count = 1;
+    Console.Write(N + "-> ");
+    while(count <= N)
+    {
+        Console.Write(count * count * count + " ");
+        count++;
+    }
+}
+Console.Write("Введите число: ");
+
+int Number = Convert.ToInt32(Console.ReadLine());
+
+if(Number > 0)
+cube(Number);
+
+else
+Console.Write("Введенное число не является положительным");
